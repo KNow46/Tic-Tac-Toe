ba7cbb6389565ac678b6ff38df389211815dda2e
KNow46 <krzysiek.nooworya@gmail.com>
2022-10-21 15:19:03 +0200

    working game

--- source.cpp
+++ source.cpp
@@ -27,24 +27,43 @@ public:
 
 class board
 {
+private:
+	int availablePlaces[9];
 public:
+	bool draw;
 	string playerTurn;
 	square squares[9];
 	board() : squares()
-	{}
+	{
+		draw = false;
+		playerTurn = "X";
+		for (int i = 0; i < 9; i++)
+		{
+			availablePlaces[i] = i;
+		}
+		
+	}
 	string iswinner() 
 	{
-		//winning conditions
+		//winning conditions horrizontaly
 		if (squares[0].getOwner() == squares[1].getOwner() && squares[1].getOwner() == squares[2].getOwner())
 			return squares[0].getOwner();
 		else if (squares[3].getOwner() == squares[4].getOwner() && squares[4].getOwner() == squares[5].getOwner())
-			return squares[0].getOwner();
+			return squares[3].getOwner();
 		else if (squares[6].getOwner() == squares[7].getOwner() && squares[7].getOwner() == squares[8].getOwner())
+			return squares[6].getOwner();
+		//winning conditions perpendicularly
+		else if (squares[0].getOwner() == squares[3].getOwner() && squares[3].getOwner() == squares[6].getOwner())
 			return squares[0].getOwner();
+		else if (squares[1].getOwner() == squares[4].getOwner() && squares[4].getOwner() == squares[7].getOwner())
+			return squares[1].getOwner();
+		else if (squares[2].getOwner() == squares[5].getOwner() && squares[5].getOwner() == squares[8].getOwner())
+			return squares[2].getOwner();
+		//winning conditions diagonally
 		else if (squares[0].getOwner() == squares[4].getOwner() && squares[4].getOwner() == squares[8].getOwner())
 			return squares[0].getOwner();
 		else if (squares[2].getOwner() == squares[4].getOwner() && squares[4].getOwner() == squares[6].getOwner())
-			return squares[0].getOwner();
+			return squares[2].getOwner();
 		else
 			return "none";
 	}
@@ -74,7 +93,73 @@ public:
 		}
 		cout << "+---------+\n";
 	}
-	void put
+	string putChar()
+	{
+		int choice;
+		bool wrongChoice = true;
+		bool isdraw = true;
+		for (int i = 0; i < 9; i++)
+		{
+			if (availablePlaces[i] != -1)
+				isdraw = false;
+		}
+		if (isdraw == true)
+		{
+			draw = true;
+			return "draw";
+		}
+		do
+		{
+			printboard();
+
+			cout << "Choose a square(";
+			for (int i = 0; i < 9; i++)
+			{
+				if (availablePlaces[i] != -1 && i != 8)
+					cout << i << ", ";
+				else if (availablePlaces[i] != -1)
+					cout << i;
+			}
+			cout << "): ";
+
+			while (scanf_s("%d", &choice) != 1 || choice < 0 || choice > 8 || getchar() != '\n')
+			{
+				cout << "\nWrong choice";
+				Sleep(2000);
+				while (getchar() != '\n')
+					;
+				printboard();
+				for (int i = 0; i < 9; i++)
+				{
+					if (availablePlaces[i] != -1 && i != 8)
+						cout << i << ", ";
+					else if (availablePlaces[i] != -1)
+						cout << i;
+				}
+				cout << "): ";
+			}
+
+			for (int i = 0; i < 9; i++)
+			{
+				if (choice == availablePlaces[i])
+					wrongChoice = false;
+			}
+			if (wrongChoice == true)
+			{
+				cout << "\nWrong choice";
+				Sleep(2000);
+			}
+			else
+			availablePlaces[choice] = -1;
+		} while (wrongChoice == true);
+		squares[choice].setOwner(playerTurn);
+		if (playerTurn == "O")
+			playerTurn = "X";
+		else
+			playerTurn = "O";
+		return iswinner();
+
+	}
 	
 
 };
@@ -84,6 +169,20 @@ public:
 int main()
 {
 	board tab;
+	
+	while (tab.putChar() == "none")
+	{
+	}
 	tab.printboard();
+	if (tab.draw != true)
+	{
+		if (tab.playerTurn == "O")
+			tab.playerTurn = "X";
+		else
+			tab.playerTurn = "O";
+		cout << endl << "Player " << tab.playerTurn << " won game";
+	}
+	else
+		cout << endl << "DRAW";
 	return 0;
 }
